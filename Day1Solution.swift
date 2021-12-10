cae57854c31e10cd10d3f68dc368dad5fb37c43b
bryce <bpenberthy@gmail.com>
2021-12-10 08:53:14 -0800

    Adding solution for step 2 in the first day's challenge

--- Day1Solution.swift
+++ Day1Solution.swift
@@ -8,20 +8,30 @@
 import Foundation
 
 class Day1Solution {
+    let WINDOW_SIZE = 3
+    
     func solution() {
         do {
-            let dataString = try String(contentsOfFile: "/Users/bryce/code/AdventOfCode/data/Day1Data.txt")
+            let dataString = try String(contentsOfFile: "/Users/bryce/code/AdventOfCode2021/data/Day1Data.txt")
             let dataValues = dataString.components(separatedBy: "\n")
             
             var counter = 0
-            var lastValue = Int.max
+            var previousValue = nil as Int?
+            var windowValues = [Int?](repeating: nil, count: WINDOW_SIZE)
             dataValues.forEach { item in
                 let currentValue = Int(item) ?? 0
-                if (currentValue > lastValue) {
-                    counter += 1
-                } else {
+                windowValues = shiftWindow(windowValues: windowValues, currentValue: currentValue)
+                let value = calculateValue(windowValues: windowValues)
+                
+                if (isWindowPopulated(windowValues: windowValues) && (previousValue != nil)) {
+                    if (value! > previousValue!) {
+                        print("\(value!) increased")
+                        counter += 1
+                    } else {
+                        print("\(value!) decreased")
+                    }
                 }
-                lastValue = currentValue
+                previousValue = value
             }
             
             print("\n")
@@ -30,4 +40,26 @@ class Day1Solution {
             print("I couldn't open the file")
         }
     }
+    
+    private func shiftWindow(windowValues: [Int?], currentValue: Int) -> [Int?] {
+        var shiftedValues = [Int?](repeating:nil, count: WINDOW_SIZE)
+        for index in 0...(WINDOW_SIZE - 2) {
+            shiftedValues[index + 1] = windowValues[index]
+        }
+        shiftedValues[0] = currentValue
+        return shiftedValues
+    }
+    
+    private func calculateValue(windowValues: [Int?]) -> Int? {
+        if (!isWindowPopulated(windowValues: windowValues)) {
+            return nil
+        }
+        return windowValues.map { $0! }.reduce(0) { result, item in result + item }
+    }
+    
+    
+    private func isWindowPopulated(windowValues: [Int?]) -> Bool {
+        return (windowValues[0] != nil) && (windowValues[1] != nil) && (windowValues[2] != nil)
+    }
+    
 }
